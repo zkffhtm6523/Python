2e6a7b3a2afa551800ee85cd327d98284ed990fc
David <zkffhtm6523@naver.com>
2021-02-13 19:26:51 +0900

    21.02.13 Quiz까지 마무리

---
+++ Lecture/rpa_basic/1_excel/16_image.py
@@ -0,0 +1,16 @@
+from openpyxl import Workbook
+from openpyxl.drawing.image import Image
+
+wb = Workbook()
+ws = wb.active
+
+img = Image("img.png")
+
+#C3 위치에 img.png 파일의 이미지를 삽입
+ws.add_image(img, "C3")
+
+wb.save("sample_image.xlsx")
+
+# pip install pillow 설치 필요
+
+#ImportError : You must install pillow to fetch image...
