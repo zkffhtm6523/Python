bad9463ddc2017fc603ee88356892d7ddfc188e4
DavidKim <zkffhtm6523@naver.com>
2021-02-14 02:22:16 +0900

    21.02.14 PC 환경설정

--- Lecture/rpa_basic/2_desktop/1_eny.py
+++ Lecture/rpa_basic/2_desktop/1_eny.py
@@ -5,3 +5,5 @@ import pyautogui
 # 현재 화면의 스크린 사이즈를 가져옴
 size = pyautogui.size()
 print(size)
+# size[0] : width
+# size[1] : height
---
+++ Lecture/rpa_basic/2_desktop/2_mouse_move.py
@@ -0,0 +1,5 @@
+import pyautogui
+
+# 마우스 이동
+# 지정한 위치로 마우스를 이동
+pyautogui.moveTo(100, 100)
