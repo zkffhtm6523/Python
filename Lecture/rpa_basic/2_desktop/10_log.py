e855a2675a317657274ae99270be47a26d746920
DavidKim <zkffhtm6523@naver.com>
2021-02-16 23:17:50 +0900

    21.02.16 로그 찍키

--- Lecture/rpa_basic/2_desktop/10_log.py
+++ Lecture/rpa_basic/2_desktop/10_log.py
@@ -2,7 +2,7 @@
 # 로그를 작성한다
 
 import logging
-
+from datetime import datetime
 # level = debug 이상 level은 다 찍어준다, critical을 적을 시 낮은 레벨은 안 보여줌
 # format %(asctime)s 시간 정보
 logging.basicConfig(level=logging.DEBUG, format="%(asctime)s [%(levelname)s] %(message)s")
@@ -15,3 +15,25 @@ logging.error("에러가 발생하였습니다. 에러 코드는....")
 logging.critical("복구가 불가능한 심각한 문제가 발생했습니다.")
 
 # 터미널과 파일에 함께 로그 남기기
+# 시간 [로그레벨] 메시지 형태로 로그를 작성
+logFormatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
+logger = logging.getLogger()
+
+# 로그 레벨 설정
+logger.setLevel(logging.DEBUG)
+
+# 스트림 (터미널)
+streamHandler = logging.StreamHandler()
+streamHandler.setFormatter(logFormatter)
+logger.addHandler(streamHandler)
+
+# 파일
+# mylogfile_20201010141011.log로 파일 생성됨
+filename = datetime.now().strftime("mylogfile_%Y%m%d%H%M%S.log")
+fileHandler = logging.FileHandler(filename, encoding="UTF-8")
+fileHandler.setFormatter(logFormatter)
+logger.addHandler(fileHandler)
+
+# 파일과 터미널에 함께 로고 남기기
+
+logger.debug("로그를 남겨보는 테스트를 진행합니다.")
