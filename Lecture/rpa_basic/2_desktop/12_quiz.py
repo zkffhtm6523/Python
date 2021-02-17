d0d8d8d80ad16ce81c58a8c8df4db78b78bd266e
DavidKim <zkffhtm6523@naver.com>
2021-02-18 00:13:34 +0900

    21.02.18 Window 마무리

--- Lecture/rpa_basic/2_desktop/12_quiz.py
+++ Lecture/rpa_basic/2_desktop/12_quiz.py
@@ -7,3 +7,52 @@
 
 # 3. 5초 대기 후 그림판 종료
 # 이 때, 저장하지 않음을 자동으로 선택하여 프로그램이 완전 종료되도록 함
+
+import pyperclip
+import sys
+import pyautogui
+# Test
+
+# pyautogui.hotkey("win","r")
+# pyautogui.write("notepad")
+# pyautogui.press("enter")
+# pyautogui.write("Hi~")
+
+# 1번 완료
+pyautogui.hotkey("win","r") # 단축키 : win + r
+pyautogui.write("mspaint")
+pyautogui.press("enter")
+
+# 그림판 나타날 때까지 대기
+pyautogui.sleep(2)
+# 그림판 1개만 띄워져 있다고 가정
+window = pyautogui.getWindowsWithTitle("제목 없음 - 그림판")[0]   
+
+if window.isMaximized == False:
+    window.maximize() # 최대화
+
+# 2번 완료
+btn_text = pyautogui.locateOnScreen("rpa_basic/2_image/btn_text.png")
+if btn_text:
+    pyautogui.click(btn_text)
+else:
+    print("찾기 실패")
+    sys.exit()
+
+# 흰 영역 클릭
+pyautogui.click(200, 200, duration=0.5)
+
+
+def my_write(text):
+    pyperclip.copy(text)
+    pyautogui.hotkey("ctrl","v")
+
+my_write("참 잘했어요")
+
+# 5초 대기
+pyautogui.sleep(0.5)
+
+# 프로그램 종료
+window.close()
+pyautogui.sleep(0.5)
+pyautogui.press("n")
