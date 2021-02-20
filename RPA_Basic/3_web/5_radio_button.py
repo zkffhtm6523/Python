c6e3e2a441cc9e2b3f7b6fee72874b2f56822159
David <zkffhtm6523@naver.com>
2021-02-20 18:33:18 +0900

    21.02.20 selennium

---
+++ RPA_Basic/3_web/5_radio_button.py
@@ -0,0 +1,6 @@
+ from selenium import webdriver
+import time
+
+browser = webdriver.Chrome('./chromedriver')
+
+browser.get("https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_input_type_radio")
