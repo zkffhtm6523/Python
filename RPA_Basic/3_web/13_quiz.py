9319d747b316c4f2f51039d9fd5b0ba438f7dc56
David <zkffhtm6523@naver.com>
2021-02-21 18:01:45 +0900

    21.02.21 [웹 자동화] 메무리

--- RPA_Basic/3_web/13_quiz.py
+++ RPA_Basic/3_web/13_quiz.py
@@ -12,3 +12,44 @@
 #     * 위 값들은 변수로 미리 저장해두세요.
 # 6. 5초 대기 후 Submit 버튼 클릭
 # 7. 5초 대기 후 브라우저 종료
+import time
+from selenium import webdriver
+
+browser = webdriver.Chrome('./chromedriver')
+browser.maximize_window()
+
+browser.get('https://www.w3schools.com')
+
+# Learn HTML 클릭
+browser.find_element_by_xpath('//*[@id="main"]/div[1]/div[1]/a[1]').click()
+# 상단 메뉴 HOW TO 클릭
+browser.find_element_by_xpath('//*[@id="topnav"]/div/div[1]/a[10]').click()
+# Contant_Form 메뉴 클릭
+# browser.find_element_by_xpath('//*[@id="leftmenuinnerinner"]/a[116]').click()
+
+# Contact Form 텍스트 2개 이상일 때 실패
+# browser.find_element_by_link_text('Contact Form').click()
+
+# 일부 텍스트를 비교하는 방법
+# browser.find_element_by_xpath('//*[@id="leftmenuinnerinner"]/a[contains(text(), "Contact")]').click()
+
+# a 태그 아래에서 Contact Form을 찾기
+browser.find_element_by_xpath('//*[@id="leftmenuinnerinner"]/a[text()="Contact Form"]').click()
+
+first_name = '나도'
+last_name = '코딩'
+country = "Canada"
+subject = "퀴즈 완료하였습니다."
+
+browser.find_element_by_xpath('//*[@id="fname"]').send_keys(first_name)
+browser.find_element_by_xpath('//*[@id="lname"]').send_keys(last_name)
+browser.find_element_by_xpath('//*[@id="country"]/option[text()="{}"]'.format(country)).click()
+browser.find_element_by_xpath('//*[@id="main"]/div[3]/textarea').send_keys(subject)
+
+time.sleep(2)
+browser.find_element_by_xpath('//*[@id="main"]/div[3]/a').click()
+
+time.sleep(2)
+browser.quit()
+
+
