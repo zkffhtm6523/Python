82a37338022d29ebc3df973a679cf6b147f91c99
DavidKim <zkffhtm6523@naver.com>
2021-02-25 23:56:30 +0900

    21.02.25 Commit

---
+++ Lecture/Python_Basic/2_Operator.py
@@ -0,0 +1,16 @@
+# 연산자
+print(2**3) # 2^3=8
+print(5%3) # 나머지 2
+print(5//3) # 몫 1
+
+print(10 > 3) # True
+print(4 >= 7) # False
+print(10 < 3) # False
+print(5 <= 5) # True
+
+print(3 == 3) # True
+print(4 == 2) # False
+print(3 + 4 == 7) # True
+
+print(1 != 3) # True
+print(not(1 != 3)) # False
--- Lecture/Python_Basic/4_if.py
+++
@@ -1,6 +0,0 @@
-#if문
-password = '333'
-if password == '444':
-    print('로그인 성공')
-else:
-    print('로그인 실패')
