3b1f8e16914a867f24aa140ec4e6c9acba9e407c
DavidKim <zkffhtm6523@naver.com>
2021-03-03 21:25:47 +0900

    21.03.03 파이썬

--- Lecture/Python_Basic/4-3.StrMethod.py
+++ Lecture/Python_Basic/4-3.StrMethod.py
@@ -8,4 +8,17 @@ print(python.upper())
 
 # 대문자인가?(boolean)
 print(python[0].isupper())
+# 문자 길이
 print(len(python))
+# 글자 대체
+print(python.replace("Python", "Java"))
+
+# n이 있는 index 위치
+index = python.index("n")
+print(index)
+
+# 찾은 위치 이후 부터 두번째 n을 찾음
+index = python.index("n", index + 1)
+print(index)
+
+print
