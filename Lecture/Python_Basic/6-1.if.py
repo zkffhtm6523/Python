def9ddea414e1081ff77d56eed6fd415800e4e8e
DavidKim <zkffhtm6523@naver.com>
2021-03-05 23:47:54 +0900

    21.03.05 if문 완료

--- Lecture/Python_Basic/6-1.if.py
+++ Lecture/Python_Basic/6-1.if.py
@@ -11,3 +11,7 @@ if temp >= 30:
     print("너무 더워요. 나가지 마세요")
 elif temp >= 10 and temp > 30:
     print("괜찮은 날씨에요")
+elif 0 <= temp < 10:
+    print("외투를 챙기세요")
+else:
+    print("너무 추워요. 나가지 마세요")
