071eba7e8e7173928ab646eabc60edcd974e4978
DavidKim <zkffhtm6523@naver.com>
2021-03-05 23:46:13 +0900

    21.03.05 if문

---
+++ Lecture/Python_Basic/6-1.if.py
@@ -0,0 +1,13 @@
+# weather = input("오늘 날씨는 어때요?")
+# if weather == "비" or weather == "눈":
+#     print("우산을 챙기세요")
+# elif weather == "미세먼지":
+#     print("마스크를 챙기세요")
+# else:
+#     print("준비물 필요 없어요.")
+
+temp = int(input("기온은 어때요?"))
+if temp >= 30:
+    print("너무 더워요. 나가지 마세요")
+elif temp >= 10 and temp > 30:
+    print("괜찮은 날씨에요")
